db336c202bf20cefda4035dfc267f221598861ec
rubin.apore <rubin.apore@gmail.com>
2017-03-29 22:45:21 +0000

    Add getCurrencies method to lib

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
 "use strict";
-
+var tap = require('gulp-tap')
 var gulp = require('gulp'),
     boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
-boilerplate({build: 'turtle', jscs: false, jshint: false});
+gulp.task('copy:data', function () {
+  console.log('running task copy:data ')
+  gulp.src('lib/data.json')
+    .pipe(tap(function(file, t) {
+    }))
+    .pipe(gulp.dest('build/lib/'));
+})
+
+boilerplate({
+  extraDefaultTasks: ['copy:data'],
+  build: 'turtle',
+  jscs: false,
+  jshint: false});
